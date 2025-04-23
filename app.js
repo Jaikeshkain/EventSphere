fc74ca6a26cdfb9a0a44cac1ec17f97ddeb77c4b
Jaikesh Kain <jaikeshkain@gmail.com>
2025-04-24 04:16:37 +0530

    Remove sensitive information from .env file

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 //pool
-const pool=require("./routes/pool")
-pool()
+const pool = require("./routes/pool");
+pool();
 
 //!connect-mongo
-const MongoStore=require("connect-mongo");
+const MongoStore = require("connect-mongo");
 //!session
-const session=require("express-session");
+const session = require("express-session");
 
 // var indexRouter = require('./routes/index');
-var homeRouter = require('./routes/home');
-const userRouter=require("./routes/user");
+var homeRouter = require("./routes/home");
+const userRouter = require("./routes/user");
 const postsRouter = require("./routes/posts");
-
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //!passport
-const passport=require("passport")
-const passportConfig = require('./configs/Passport');
-const errorHandler = require('./middlewares/errorHandler');
+const passport = require("passport");
+const passportConfig = require("./configs/Passport");
+const errorHandler = require("./middlewares/errorHandler");
 
 //?session middleware
-app.use(session({
-  secret:"keyboard cat",
-  resave:false,
-  saveUninitialized:false,
-  cookie:{
-    maxAge:24*60*60*1000
-  },
-  store:MongoStore.create({mongoUrl:"mongodb+srv://hjoy118181689:7F8cuofPecjq2YuL@fullstack-blog.h3ljh9t.mongodb.net/fullstack-blog?retryWrites=true&w=majority&appName=Fullstack-Blog"})
-}))
-passportConfig(passport)
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://hjoy118181689:7F8cuofPecjq2YuL@fullstack-blog.h3ljh9t.mongodb.net/fullstack-blog?retryWrites=true&w=majority&appName=Fullstack-Blog",
+    }),
+  })
+);
+passportConfig(passport);
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 
 // 🔧 Add this before any routes or error handlers
 app.use((req, res, next) => {
@@ -56,29 +62,28 @@ app.use((req, res, next) => {
   next();
 });
 
-
 // app.use('/', indexRouter);
-app.use('/', homeRouter);
-app.use("/auth",userRouter)
-app.use("/posts",postsRouter)
+app.use("/", homeRouter);
+app.use("/auth", userRouter);
+app.use("/posts", postsRouter);
 
 //!Error Handler
-app.use(errorHandler)
+app.use(errorHandler);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- configs/Passport.js
+++ configs/Passport.js
@@ -20,29 +20,34 @@ module.exports = function(passport) {
     }));
 
     // Google Strategy
-    passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback"
-    }, async (accessToken, refreshToken, profile, done) => {
-        try {
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL: "/auth/google/callback",
+        },
+        async (accessToken, refreshToken, profile, done) => {
+          try {
             const existingUser = await User.findOne({ googleId: profile.id });
 
             if (existingUser) return done(null, existingUser);
 
             const newUser = new User({
-                googleId: profile.id,
-                username: profile.displayName,
-                email: profile.emails[0].value,
-                profilePic: profile.photos[0].value
+              googleId: profile.id,
+              username: profile.displayName,
+              email: profile.emails[0].value,
+              profilePic: profile.photos[0].value,
             });
 
             await newUser.save();
             return done(null, newUser);
-        } catch (err) {
+          } catch (err) {
             return done(err);
+          }
         }
-    }));
+      )
+    );
 
     passport.serializeUser((user, done) => done(null, user.id));
 
